1623f9fb42db0b50d41fae6f0e6f43980f3566a4
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-08-05 22:19:21 +0200

    Repair error at routes

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 const db = require('./db');
+
+const app = express();
+
+// import routes
 const testimonialsRouter = require('./routes/testimonials.routes');
 const concertsRouter = require('./routes/concerts.routes');
 const seatsRouter = require('./routes/seats.routes');
 
-const app = express();
-
 app.use(cors());
-
-
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 /* ENDPOINTS  */
 
-app.use('/api/testimonials', testimonialsRouter);
-app.use('/api/concerts', concertsRouter);
-app.use('/api/seats', seatsRouter);
+app.use('/api', testimonialsRouter);
+app.use('/api', concertsRouter);
+app.use('/api', seatsRouter);
 
 /* ------------------------------------------------------------ */
 app.use((req, res) => {
