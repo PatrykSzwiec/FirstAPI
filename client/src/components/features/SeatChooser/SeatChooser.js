22671027de748a8918fa520c5f77d539bd9593cf
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-08-24 20:33:26 +0200

    Add the counter of free seats at real time

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -16,61 +16,55 @@ const getSocketConnectionUrl = () => {
 };
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
-  const [socket, setSocket] = useState(null);
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
 
+  const [socket, setSocket] = useState(null);
+
   useEffect(() => {
-    // Conditionally choose the socket connection URL based on environment
+    // Establishing the socket connection
     const socketConnectionUrl = getSocketConnectionUrl();
     const newSocket = io(socketConnectionUrl);
+
     setSocket(newSocket);
 
     // Socket.IO event listener for 'connection'
     newSocket.on('connect', () => {
       console.log('New socket connected:', newSocket.id); // Log the socket ID
 
+      // Event listener for updated seats data
       newSocket.on('seatsUpdated', handleSeatsUpdated);
     });
 
+    // Callback for handling updated seats data
     const handleSeatsUpdated = (updatedSeats) => {
       console.log('Updated seats:', updatedSeats);
       dispatch(loadSeats(updatedSeats));
     };
-
+   
     // Clean up the socket connection when the component unmounts
     return () => {
       newSocket.disconnect();
       newSocket.off('seatsUpdated', handleSeatsUpdated);
     };
-  }, [dispatch]);
+  }, [dispatch, chosenDay]);
 
-  const handleReserve = (seat) => {
-    const reservationData = {
-      day: chosenDay,
-      seat: seat,
-      client: 'Your Client Name', // Replace with actual client info
-      email: 'your@email.com',    // Replace with actual email
-    };
-    
-    // Emit the reservation data to the server using the 'reserveSeat' event
-    socket.emit('reserveSeat', reservationData);
-  };
-
-  useEffect(() => {
-    dispatch(loadSeatsRequest());
-  }, [dispatch, chosenSeat]);
-
+  // Function to check if a seat is already taken
   const isTaken = (seatId) => {
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
 
+  // Function to render seat buttons based on their availability
   const prepareSeat = (seatId) => {
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
     else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
   }
+
+  // Counting taken and free seats
+  const takenSeats = seats.filter(seat => seat.day === chosenDay).length;
+  const freeSeats = 50 - takenSeats;
 
   return (
     <div>
@@ -80,6 +74,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <p>Free seats: {freeSeats}/50</p>
     </div>
   )
 }
